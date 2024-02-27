df696db2f7fa6ecc8d89a79b275e8796c6855d0d
EduxMontivero <40614697+EduxMontivero@users.noreply.github.com>
2024-02-26 23:38:42 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  echo "php.";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<br>
+	<center>
+		<h1>Hola</h1>
+	
+	<p>
+		Como estas?
+	</p>
+	</center>
+	<br>
+</body>
+</html>
